71963869014abc0c23dc8185111e1265d70fd6aa
Paulo <paulorcpassos@gmail.com>
2022-11-04 08:25:38 -0300

    Changing testing_goalkeeperlab.m location

--- pre_functions/testing_goalkeeperlab.m
+++ pre_functions/testing_goalkeeperlab.m
@@ -1,13 +1,21 @@
-%HomeCode
-close all
-home = '/home/paulo/Documents/PauloPosDoc/Pos-Doc/BagOfCodesAndData';
-addpath(genpath(home))
-load([home '/ExperimentData/ColetaINDCpilotoDownsampled/PreprocessedData/vol01sr1000.mat'])
-load([home '/ExperimentData/ColetaINDCpilotoDownsampled/Technical Aspects/chanlocs.mat'])
+% Setting paths
+working_path = '/home/roberto/Documents/Pos-Doc/pd_paulo_passos_neuromat';
+
+% Adding paths
+addpath(genpath(working_path))
+
+% Loading data
+%load([working_path '<PATH TO DATA MATRIX>'])
+%load([working_path '<PATH TO DATA MATRIX>'])
+
+% Script Description:
+%
+% The following script stores should run after uploading EEG data from the
+% goalkeeper lab. It prepare the parameters and data for the goalkeeper
+% lab initiation.
+
+
 fs = 1000;
 ntrials = 1500;
-data(:,6)= 1*ones(ntrials,1);
-% [data, channels, EEGtimes, EEGsignals] = to_datamatrix(ALLEEG, ntrials,
-% fs, 1, 7); not necessary after loading
-
-GoalkeeperLab(data,[home '/Git'],ntrials,EEGsignals,fs,channels)
+[data, channels, EEGtimes, EEGsignals] = to_datamatrix(ALLEEG, ntrials, fs, 1, 7, 1);
+GoalkeeperLab(data,[working_path '/research_codes'],ntrials,EEGsignals,fs,channels)
